6addc31c1f45980514486e58ed06c1d5e66aa784
mateito135798 <112591572+mateito135798@users.noreply.github.com>
2022-10-17 17:43:10 -0500

    Add files via upload

---
+++ lamat.js
@@ -0,0 +1,25 @@
+var j1=localStorage.getItem("user1");
+var j2=localStorage.getItem("user2");
+var p1=0;
+var p2=0;
+document.getElementById("jugador1").innerHTML=j1+":"+p1;
+document.getElementById("jugador2").innerHTML=j2+":"+p2;
+document.getElementById("pregunta").innerHTML="turno de preguntar:"+j2;
+//hola
+document.getElementById("responde").innerHTML="turno de responder:"+j1;
+function porfin(){
+
+}
+function a(){
+    var q=document.getElementById("n1").value;
+    var w=document.getElementById("n2").value;
+    var e=parseInt(q)*parseInt(w);
+
+
+
+
+/
+
+
+    }
+
